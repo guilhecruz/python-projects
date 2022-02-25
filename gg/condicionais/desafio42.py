51da79ad3676d3e27980938d687a36b4807b8405
Guilherme Cruz <guilherme_souza_@hotmail.com>
2022-02-25 16:54:39 -0300

    desafio42.py

--- gg/condicionais/desafio42.py
+++ gg/condicionais/desafio42.py
@@ -10,12 +10,12 @@ print(f"Os segmentos submetidos foram {a1}, {a2} e {a3}")
 
 if a1 < a4 and a2 < a5 and a3 < a6:
     print(f"Os angulos {a1}, {a2} e {a3}, satisfazem as condições para criação de um triângulo")
-    if a1 == a2 and a2 == a3:
+    if a1 == a2 == a3:
         print("O triângulo formado é um triângulo Equilátero")
-    if a1 == a2 and a2 != a3:
-        print("O triângulo formado é um triângulo Isósceles")
-    if a1 != a2 and a2 != a3:
+    if a1 != a2 != a3 != a1 :
         print("O triângulo formado é um triângulo Escaleno")
+    else:
+        print("O triângulo formado é um triângulo Isósceles")
 
 else:
     print(f"Os angulos {a1}, {a2} e {a3} não satisfaem as condições para a criação de um triângulo")
