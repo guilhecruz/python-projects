a5e5559fc6bab07a27657a56efbc14d7223aeab2
Guilherme Cruz <guilherme_souza_@hotmail.com>
2022-03-04 13:52:05 -0300

    repeticao

--- gg/repeticao/desafio66.py
+++ gg/repeticao/desafio66.py
@@ -1,3 +1,9 @@
-n = 0
-while n != 999:
-    n = int(input("Digite um número: "))
+num = soma = cont = 0
+
+while True:
+    num = int(input("Digite um valor (999 para parar): "))
+    if num == 999:
+        break
+    soma += num
+    cont +=1
+print(f"A soma dos {cont} valores é igual a: {soma}")
--- gg/repeticao/desafio71.py
+++ gg/repeticao/desafio71.py
@@ -1,6 +1,19 @@
+
 '''
 1 - perguntar ao user qual o valor que deseja sacar
 2 - informar quantas cédulas de cada valor serão entregues
 3 - cédulas de 50, 20, 10 e 1
 '''
-https://prod.liveshare.vsengsaas.visualstudio.com/join?AFB1F7AAEE2ACBCD355BCD02495F596C98E8
+valor = cinq = vin = dez = uni = cedulas = 0
+condição = "s"
+
+while condição == "s":
+    valor = int(input("Qual o valor que você deseja sacar? "))
+    cinq = valor.index[0]
+    print(cinq)
+
+#print('''Serão entregues:
+#{cinq} notas de 50
+#{vin} notas de 20
+#{dez} notas de 10
+#{uni} notas de 1''')
