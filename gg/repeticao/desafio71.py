fb3b3a7370ed7f12e30ded0425e15634b683c44f
Guilherme Cruz <guilherme_souza_@hotmail.com>
2022-03-04 12:30:25 -0300

    repeticao

--- gg/repeticao/desafio71.py
+++ gg/repeticao/desafio71.py
@@ -1,5 +1,6 @@
 '''
 1 - perguntar ao user qual o valor que deseja sacar
 2 - informar quantas cédulas de cada valor serão entregues
-
+3 - cédulas de 50, 20, 10 e 1
 '''
+https://prod.liveshare.vsengsaas.visualstudio.com/join?AFB1F7AAEE2ACBCD355BCD02495F596C98E8
