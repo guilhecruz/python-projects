e11071e58351b648a711f70b66188af3a7d73b21
Guilherme Cruz <guilherme_souza_@hotmail.com>
2022-03-07 14:07:41 -0300

    repeticao

--- gg/repeticao/desafio58.py
+++ gg/repeticao/desafio58.py
@@ -1,6 +1,6 @@
 from random import randint
 from re import T
-computador = randin(0,10)
+computador = randint(0,10)
 
 print("Sou seu computador.. Acabei de pensar em um número entre 0 e 10\n Será que você consegue adivinhar qual foi? ")
 acertou = False
@@ -15,7 +15,7 @@ while not acertou:
             print("Mais... Tente mais uma vez")
         else:
             print("Menos... Tente mais uma vez")
-print(f"Acertou com {palpites} tentativas, mizeravi")
+print(f"Acertou com {palpite} tentativas, mizeravi")
 
 
 
