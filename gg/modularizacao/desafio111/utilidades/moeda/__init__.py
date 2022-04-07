d6070753049f9c76b6ca995b737f843b48276ce4
Guilherme de Souza Cruz <guilherme_souza_@hotmail.com>
2022-04-07 09:53:12 -0300

    pasta de modulos

---
+++ gg/modularizacao/desafio111/utilidades/moeda/__init__.py
@@ -0,0 +1,25 @@
+def dobro(n):
+    return n * 2
+
+def aumentar(n, p):
+    return n * (p/100) + n
+
+def diminuir(n, p):
+    return (n * (p/100) - n)*-1
+
+def metade (n):
+    return n / 2
+
+def moeda(n):
+    n.replace('.', ',')
+
+def resumo(n, a=10, r=5):
+    print('-'*30)
+    print(f'RESUMO DO VALOR'.center(30))
+    print('-'*30)
+    print(f'O preço analisado é R${n}')
+    print(f'A metade de {n} é {metade(n)}')
+    print(f'O dobro de {n} é {dobro(n)}')
+    print(f'Aumentando em {a}%, temos {aumentar(n, a)}')
+    print(f'Diminuindo em {r}%, temos {diminuir(n, r)}')
+    print('-'*30)
---
+++ gg/modularizacao/intro.py
@@ -0,0 +1,7 @@
+import uteis
+from desafio111.utilidades import moeda
+from desafio112.utilidades import dado
+
+p = dado.leiaDinheiro('Digite o preço R$: ')
+moeda.resumo(p,35,22)
+#174 horas
